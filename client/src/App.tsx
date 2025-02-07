29122e6877b5ab6c5dc7b1e6d31884451c2a3a3e
chairielazizi <azizihr2000@gmail.com>
2025-02-07 21:46:43 +0800

    simple UI for add note, make post request function in client, fix cors error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState("");
 
+  async function handleAddNote(e: React.FormEvent) {
+    e.preventDefault();
+    await fetch("http://localhost:3000/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+      }),
+    });
+    setTitle("");
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <form onSubmit={handleAddNote}>
+        <label htmlFor="deck-title ">Notes</label>
+        <input
+          id="deck-title"
+          type="text"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button>Add Note</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import express, { Request, Response } from "express";
 const app = express();
 import mongoose from "mongoose";
 const PORT = 3000;
+import cors from "cors";
 
 const databaseUrl = process.env.DATABASE_URL;
 if (!databaseUrl) {
@@ -20,6 +21,15 @@ const db = mongoose
     });
   })
   .catch((err) => console.error(err));
+
+//fix cors error
+app.use(
+  cors({
+    origin: "http://localhost:5173", // allow requests from this origin
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // allow these methods
+    allowedHeaders: ["Content-Type", "Authorization"], // allow these headers
+  })
+);
 
 // accept json request
 app.use(express.json());
